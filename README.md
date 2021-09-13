2243f0e9cf217cdb0f51c80a89978fbb4106f462
John Ballesteros <johnshift.dev@gmail.com>
2021-09-13 12:49:07 +0800

    redux-typescript template + strict eslint

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + Redux Toolkit in Typescript
+Rewrite Todo app using typescript:
+  - use `--template redux-typescript`
+  - implement eslint for typescript
+  - explicit return types
+  - declarative redux typescript
---
+++ src/app/hooks.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from './store';
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
